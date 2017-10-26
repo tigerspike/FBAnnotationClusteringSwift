3a428a06910abfd24ee82de621c087bac96d4a18
Pierre-Michel Villa <contact@ant-robot.com>
2017-10-26 17:45:16 +0200

    no message

---
+++ FBAnnotationClusteringSwift/FBAnnotationClusteringSwift/FBAnnotationClusteringSwift.h
@@ -0,0 +1,19 @@
+//
+//  FBAnnotationClusteringSwift.h
+//  FBAnnotationClusteringSwift
+//
+//  Created by Pierre-Michel Villa on 26/10/2017.
+//  Copyright © 2017 Ant Robot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBAnnotationClusteringSwift.
+FOUNDATION_EXPORT double FBAnnotationClusteringSwiftVersionNumber;
+
+//! Project version string for FBAnnotationClusteringSwift.
+FOUNDATION_EXPORT const unsigned char FBAnnotationClusteringSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBAnnotationClusteringSwift/PublicHeader.h>
+
+
